accbc26600f0d31cc4db407983dbc7fbe9f59c08
manzidime <malondascript@gmail.com>
2020-09-14 15:44:29 +0200

    base

--- v_all_contribuables_contains_vehicules.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `v_all_contribuables_contains_vehicules`(`id_contribuable`, `nom`) VALUES (8, 'EFAMBE');
-INSERT INTO `v_all_contribuables_contains_vehicules`(`id_contribuable`, `nom`) VALUES (9, 'malonda');
-INSERT INTO `v_all_contribuables_contains_vehicules`(`id_contribuable`, `nom`) VALUES (10, 'malonda');
-INSERT INTO `v_all_contribuables_contains_vehicules`(`id_contribuable`, `nom`) VALUES (14, 'MANGUNGU KASSAY ALLAI');
-INSERT INTO `v_all_contribuables_contains_vehicules`(`id_contribuable`, `nom`) VALUES (15, 'MALONDA NZITA Dieu-Merci');
-INSERT INTO `v_all_contribuables_contains_vehicules`(`id_contribuable`, `nom`) VALUES (19, 'john');

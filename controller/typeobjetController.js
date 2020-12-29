99a925819d48811f632f68f9fca67c1afcfff282
manzidime <61105347+manzidime@users.noreply.github.com>
2020-12-29 12:27:03 +0100

    erererere

--- controller/typeobjetController.js
+++
@@ -1,5 +0,0 @@
-const TypeObjet = require('./../model/typeObjetModel');
-const globalController = require('./globalController');
-
-//Get all
-exports.getAll = globalController.getAllRows(TypeObjet);
